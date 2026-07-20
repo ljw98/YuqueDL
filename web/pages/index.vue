ca7074bc83cc577afde463dd28d3ebd1ce69adc7
Ada <ada@local>
2026-07-20 05:36:36 +0000

    style(web): increase el-segmented item height to 44px

--- web/pages/index.vue
+++ web/pages/index.vue
@@ -323,7 +323,7 @@ onBeforeUnmount(closeEs)
   --el-segmented-item-selected-color: #fff;
   --el-segmented-item-selected-bg-color: #31cc79;
   --el-border-radius-base: 16px;
-  --el-segmented-item-height: 36px;
+  --el-segmented-item-height: 44px;
 }
 .mode-segmented .el-segmented__item.is-selected {
   color: #fff;
